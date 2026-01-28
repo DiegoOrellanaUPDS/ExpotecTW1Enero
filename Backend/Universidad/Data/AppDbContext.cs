afe67498d9d6ab51e2d03ef68b461f75d59fade4
Coxibius <victormcox26@gmail.com>
2026-01-28 10:07:05 -0400

    Feature: Limpieza y Objetos Perdidos completa

--- Backend/Universidad/Data/AppDbContext.cs
+++ Backend/Universidad/Data/AppDbContext.cs
@@ -7,9 +7,13 @@ namespace Universidad.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+           
         }
 
-        // Define tus DbSet aquí
+        
         public DbSet<Persona> Estudiantes { get; set; }
+
+         public DbSet<Universidad.Entidades.LimpiezaInsumo> LimpiezaInsumos { get; set; }
+          public DbSet<Universidad.Entidades.ObjetoPerdido> ObjetosPerdidos { get; set; }
     }
 }
