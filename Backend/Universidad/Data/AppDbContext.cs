1f1277dc280f6e1e4cbe9e3d05c4e71ff93a8441
MrRiky23 <ricardofrontanillasalazar@gmail.com>
2026-01-28 09:31:43 -0400

    aporte entidadad y controlador

--- Backend/Universidad/Data/AppDbContext.cs
+++ Backend/Universidad/Data/AppDbContext.cs
@@ -11,5 +11,39 @@ namespace Universidad.Data
 
         // Define tus DbSet aquí
         public DbSet<Persona> Estudiantes { get; set; }
+
+
+        public DbSet<Profesor> Profesores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Profesor>(entity =>
+            {
+                entity.ToTable("Profesores");
+                entity.HasKey(e => e.Id);
+            
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(100);
+                
+                entity.Property(e => e.Categoria)
+                    .IsRequired()
+                    .HasMaxLength(50);
+                
+                entity.Property(e => e.Correo)
+                    .IsRequired()
+                    .HasMaxLength(100);
+                
+                entity.Property(e => e.Especialidad)
+                    .HasMaxLength(200);
+                
+                entity.Property(e => e.FechaCreacion)
+                    .HasDefaultValueSql("GETDATE()");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
+
 }
+
