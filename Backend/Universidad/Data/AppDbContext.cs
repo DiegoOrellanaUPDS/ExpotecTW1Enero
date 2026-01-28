9c999d5e346d3888d52a9b404a7fcdce14b338b5
DiegoOrellanaUPDS <tj.diego.orellana.l@upds.net.bo>
2026-01-28 07:52:25 -0400

    Inicio del proyecto

---
+++ Backend/Universidad/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Entidades;
+
+namespace Universidad.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Define tus DbSet aquí
+        public DbSet<Persona> Estudiantes { get; set; }
+    }
+}
---
+++ Backend/Universidad/Entidades/Persona.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Entidades
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string CI { get; set; }
+        public string Estado { get; set; }
+    }
+}
