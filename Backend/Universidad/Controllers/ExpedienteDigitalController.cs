649bb0f065c827db5c26b57f710295e9ea29310e
Manzanilla2510 <manzanillamancilla05@gmail.com>
2026-01-29 00:29:02 -0400

    Solucion de conflictos de entidades y controladores

--- Backend/Universidad/Controllers/ExpedienteDigitalController.cs
+++ Backend/Universidad/Controllers/ExpedienteDigitalController.cs
@@ -1,5 +1,5 @@
 using Entidades;
-using Entities;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Data;
--- Backend/Universidad/Controllers/InscripcionController.cs
+++ Backend/Universidad/Controllers/InscripcionController.cs
@@ -1,5 +1,5 @@
 using Data;
-using Entities;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Entidades;
