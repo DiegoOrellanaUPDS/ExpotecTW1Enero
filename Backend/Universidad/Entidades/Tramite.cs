8df52101fa6f59cec0eb3289516ed3138fa4f61e
Adry970 <adriana.gallardo.jr@gmail.com>
2026-01-29 00:12:26 -0400

    área de trámites y archivos con entidades y controladores

---
+++ Backend/Universidad/Entidades/Tramite.cs
@@ -0,0 +1,10 @@
+namespace Universidad.Core.Entidades
+{
+    public class Tramite
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
